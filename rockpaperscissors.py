812de8f198a75368ef96a508d1ce751fd8508b9b
GoldDominik893 <68468388+GoldDominik893@users.noreply.github.com>
2022-05-20 19:45:43 +0100

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,59 @@
+from random import randint
+from time import sleep
+scorecomputer = 0
+scoreuser = 0
+chq = ""
+while scorecomputer<3 or scoreuser<3:
+  opponent = randint(1,3)
+  
+  print("Rock, Paper or Scissors?")
+  print("Computer score:",scorecomputer,"/ Your score:",scoreuser)
+  rps = input("Answer here: ")
+
+  if rps == "rock":
+    rps = 1
+  elif rps == "paper":
+    rps = 2
+  elif rps == "scissors":
+    rps = 3
+
+  if opponent == 1:
+    chq = "rock"
+  elif opponent == 2:
+    chq = "paper"
+  elif opponent == 3:
+    chq = "scissors"
+
+  if rps == opponent:
+    print("tie i also did",chq)
+    scorecomputer = scorecomputer+1
+    scoreuser = scoreuser+1
+  elif rps == 1 and opponent == 2:
+    print("i beat you i did",chq)
+    scorecomputer = scorecomputer+1
+  elif rps == 1 and opponent == 3:
+    print("you beat me i did",chq)
+    scoreuser = scoreuser+1
+  elif rps == 2 and opponent == 1:
+    print("you beat me i did",chq)
+    scoreuser = scoreuser+1
+  elif rps == 2 and opponent == 3:
+    print("i beat you i did",chq)
+    scorecomputer = scorecomputer+1
+  elif rps == 3 and opponent == 1:
+    print("you beat me i did",chq)
+    scoreuser = scoreuser+1
+  elif rps == 3 and opponent == 2:
+    print("you beat me i did",chq)
+    scoreuser = scoreuser+1
+  else:
+    print("you didnt put a valid answer.")
+
+if scorecomputer > scoreuser:
+  print("The computer won")
+elif scorecomputer < scoreuser:
+  print("You won")
+elif scorecomputer == scoreuser:
+  print("It was a tie")
+print("Closing in 10 seconds")
+sleep(10)
